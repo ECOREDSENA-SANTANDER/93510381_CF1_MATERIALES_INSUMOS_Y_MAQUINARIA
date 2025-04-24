417b9492d69e5274ee8966e7d4aef480104f9723
IngLeoCastellanos <ingleonardocastellanos@gmail.com>
2025-04-24 17:37:03 -0500

    Avance_video

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -21,8 +21,8 @@
       .col-lg-12
         figure
           .video
-            iframe(width="560" height="315" src="https://www.youtube.com/embed/vdPrCjWJSHo?si=X4NxENRo3LLXAua_" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
-        figcaption Falta link del video
+            iframe(width="560" height="315" src="https://www.youtube.com/embed/uiIgalCSDtY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+        figcaption Video: fibras e hilos - introducción
 
 </template>
 
